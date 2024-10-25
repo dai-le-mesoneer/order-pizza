91da64f8f78a204675657dbd704c1172a97b8a9e
Dai Le <dai.le@mesoneer.io>
2024-10-25 17:20:41 +0700

    feat: build docker image for project

--- order-pizza-api/src/main/java/com/assignment/pizza/OderPizzaApiApplication.java
+++ order-pizza-api/src/main/java/com/assignment/pizza/OderPizzaApiApplication.java
@@ -1,14 +1,25 @@
 package com.assignment.pizza;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 @RequiredArgsConstructor
-public class OderPizzaApiApplication {
+@Log4j2
+public class OderPizzaApiApplication implements CommandLineRunner {
+
+	private final PasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(OderPizzaApiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		log.error(passwordEncoder.encode("123456"));
 	}
 }
